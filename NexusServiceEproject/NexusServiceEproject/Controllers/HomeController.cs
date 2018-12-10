e301323f8d2db7faaabcb429b21d9159be11dca1
slkd989 <slkd7700@gmail.com>
2018-12-11 01:03:23 +0500

    desgin done with login

---
+++ NexusServiceEproject/NexusServiceEproject/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using NexusServiceEproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NexusServiceEproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        tbl_loginEntities db = new tbl_loginEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Pay()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult bbhome()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult products()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Packs()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ReportIssues()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult logout()
+        {
+            Session.RemoveAll();
+            Session.Abandon();
+            TempData.Remove("username");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(tbl_usersignup user)
+        {
+            tbl_usersignup u = db.tbl_usersignup.Where(x => x.u_Eamil == user.u_Eamil && x.u_Password == user.u_Password).SingleOrDefault();
+            if (u != null)
+            {
+                Session["u_id"] = u.u_id;
+                TempData["username"] = u.u_name;
+                TempData.Keep();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["Valid"] = "Invalid user or Password";
+            }
+            TempData.Keep();
+
+
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ login.sql
@@ -0,0 +1,19 @@
+create database tbl_login
+use tbl_login
+
+create table  tbl_usersignup
+(
+u_id int identity primary key,
+u_name nvarchar(50) not null,
+u_Eamil nvarchar(50) not null,
+u_Password nvarchar(50) not null
+)
+
+
+select * from tbl_usersignup
+
+insert into tbl_usersignup
+values('salman','sl@gmail.com','test')
+
+insert into tbl_usersignup
+values('slkd','sl@yahoo.com','55123')
